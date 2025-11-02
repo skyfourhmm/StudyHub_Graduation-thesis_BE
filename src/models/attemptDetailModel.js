bb72fe2cab77fe5060c44c2a0a6c71ce609eaece
skyfourhmmm <nguyenthientu413@gmail.com>
2025-11-02 18:15:27 +0700

    fix attempt

--- src/models/attemptDetailModel.js
+++ src/models/attemptDetailModel.js
@@ -1,4 +1,4 @@
-const AttemptDetail = require("../schemas/AttemptDetail");
+const AttemptDetail = require("../schemas/attemptDetail");
 const TestAttempt = require("../schemas/TestAttempt");
 
 // Tạo mới bản ghi AttemptDetail
